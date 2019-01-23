b1b2c0ee71ff4cf955f68b5a03db101978c29009
lively-teacher-9822 <ccrive10@gmail.com>
2019-01-23 04:02:30 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,17 @@
+#method that yields members of a collection to a block
+#Control the return value of a method that uses yield such that it returns a new collection.
+
+def my_collect(array)
 
 
+i = 0
+
+myNewCollection = []
+
+while i < array.length
+  myNewCollection << yield(array[i])
+i += 1
+
+ end
+ myNewCollection
+end
