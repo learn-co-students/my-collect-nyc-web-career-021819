18ef753fdc13a56fb6e35bba2e126c73f45ca0b9
Jessy Hu <jesshuy12@gmail.com>
2019-02-02 06:08:12 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,10 @@
-
+def my_collect(array)
+  i = 0
+  modified_collection = []
+    while i < array.length
+      modified_collection.push(yield(array[i]))
+    i += 1
+  end
+  return modified_collection
+end
 
