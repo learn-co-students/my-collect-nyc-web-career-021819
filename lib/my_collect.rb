dffd109d02538fec1a9511255290d1212eba0547
Nathaniel Cain <nathaniel2897@gmail.com>
2019-02-18 01:09:08 -0500

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,14 @@
+array = ["Tim Jones", "Tom Smith", "Jim Campagno"]
+collection = ['ruby', 'javascript', 'python', 'objective-c']
+def my_collect(array)
+  collection_array = []
+  i = 0
+  while i < array.length
+    collection_array << yield(array[i])
+    i = i + 1
+  end
+  collection_array
+end
 
-
+my_collect(array) {|name| name.split(" ").first}
+my_collect(collection) {|lang| lang.upcase}
